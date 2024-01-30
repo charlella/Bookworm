26c3aea2fb2469e7f964ca4de15464587f43ca8f
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-30 10:09:54 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by charlene hoareau on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
